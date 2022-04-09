f24864d61a6108cf5331e0585e6e9aae453e6429
GitMrYu <100071487+GitMrYu@users.noreply.github.com>
2022-04-09 11:31:20 +0800

    Update Parkour.cpp

--- Parkour/Parkour.cpp
+++ Parkour/Parkour.cpp
@@ -2,36 +2,56 @@
 #include <easy2d/easy2d.h>
 using namespace easy2d;
 
-Scene* scene;
+Scene* game;
 
-void EnterScene() {
-	scene = gcnew Scene;//新建场景
-	auto background = gcnew Sprite(L"res//Pic//B.png");//新建背景
-	background->setPos(Window::getSize() / 2);//生成位置
-	background->setScale(1.0f, 1.0f);
-	scene->addChild(background);//将背景添加到场景中
-	SceneManager::enter(scene);//进入场景
-}
-bool game() {
-	auto game = gcnew Scene;//新建场景
-	SceneManager::enter(game);//进入场景
-	auto gameBackGround = gcnew Sprite(L"res//Pic//B.png");//新建背景
-	scene->addChild(gameBackGround);//将背景添加到场景中
-	gameBackGround->setPos(Window::getSize() / 2);//生成位置
-
+void Ground() {
 	auto ground1 = gcnew Sprite(L"res//Pic//ground.png");
-	scene->addChild(ground1);
+	ground1->setPos(320, 410);
+	game->addChild(ground1);
+	auto ground2 = gcnew Sprite(L"res//Pic//ground.png");
+	ground2->setPos(960, 410);
+	game->addChild(ground2);
+	auto moveBy = gcnew MoveBy(3, Vector2(-640, 0));
+	auto tpBy = gcnew MoveBy(0, Vector2(640, 0));
+	auto sequence = gcnew Sequence({ moveBy,tpBy });
+	auto loop = gcnew Loop(sequence);
+	auto loop2 = gcnew Loop(sequence);
+	ground1->runAction(loop);
+	ground2->runAction(loop);
+}
+
+bool RunGame() {
+	game = gcnew Scene;//新建场景
+	SceneManager::enter(game);//进入场景
+	
+	auto rotateBy = gcnew RotateBy(1, 10);//创建旋转动画
+	auto alwaysRB = gcnew Loop(rotateBy);//创建一直旋转动画
+
+	auto gameBackGround = gcnew Sprite(L"res//Pic//B.png");//新建背景
+	gameBackGround->runAction(alwaysRB);
+	game->addChild(gameBackGround);//将背景添加到场景中
+	gameBackGround->setPos(Window::getSize() / 2);//生成位置
+	Ground();
 	return true;
 }
 int main() {
-	MusicPlayer::preload(L"res//Mus//Aasee.wav");//预加载音乐
+	Logger::showConsole(1);//隐藏或显示控制台
+	Renderer::showFps(1);
+	Window::setIcon(100);
 
-	Logger::showConsole(1);//隐藏控制台
+	MusicPlayer::preload(L"res//Mus//Aasee.wav");//预加载音乐
 	
 	if (Game::init(L"DUNKIRK", 640, 480)) {
 		Node::setDefaultAnchor(0.5f, 0.5f);
 		//以下是游戏代码
-		EnterScene();
+
+		auto scene = gcnew Scene;//新建场景
+		auto background = gcnew Sprite(L"res//Pic//B.png");//新建背景
+		background->setPos(Window::getSize() / 2);//生成位置
+		background->setScale(1.0f, 1.0f);
+		scene->addChild(background);//将背景添加到场景中
+		SceneManager::enter(scene);//进入场景
+
 		MusicPlayer::play(L"res//Mus//Aasee.wav", -1);//播放音乐
 		auto startbutton = gcnew Button;//开始按钮
 		startbutton->setAnchor(0.5f, 0.5f);//设置中心点
@@ -39,7 +59,7 @@ int main() {
 		startbutton->setSelected(gcnew Sprite(L"res//Pic//start2.png"));//选择时
 		startbutton->setPos(Window::getWidth() / 2, Window::getHeight() - 150);//生成位置
 		auto Pcallback = []() {
-			game();
+			RunGame();
 		};
 		startbutton->setClickFunc(Pcallback);//设置回调函数
 		scene->addChild(startbutton);// 将按钮加入场景
